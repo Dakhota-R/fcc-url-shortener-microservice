13d67171c853206ee8172509718c19ea7df75ebe
Diq_Yoda <166868070+Dakhota-R@users.noreply.github.com>
2024-12-17 13:45:49 -0600

    feat/post-test

--- index.js
+++ index.js
@@ -19,6 +19,10 @@ app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
 });
 
+app.post('/api/shorturl', function(req, res, next){
+  res.json({ success: 'true' })
+})
+
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
 });
